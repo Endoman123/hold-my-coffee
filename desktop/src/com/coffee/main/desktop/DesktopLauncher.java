19144c4be32ead273939953b2e9dec7a70c96c65
Jared Tulayan <2lyNJ.E@gmail.com>
2017-05-16 17:06:54 -0400

    First commit

---
+++ desktop/src/com/coffee/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.coffee.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coffee.main.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Application(), config);
+	}
+}
