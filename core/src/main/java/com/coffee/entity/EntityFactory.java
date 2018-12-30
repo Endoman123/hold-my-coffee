5af934236c4acb5eebac4e9b106e40ac2118dce1
Jared Tulayan <2lyNJ.E@gmail.com>
2018-12-30 14:04:01 -0500

    Revert boss rotSpeed to 2

--- core/src/main/java/com/coffee/entity/EntityFactory.java
+++ core/src/main/java/com/coffee/entity/EntityFactory.java
@@ -1416,7 +1416,7 @@ public class EntityFactory {
         final GUIComponent GUI = new GUIComponent();
 
         // Initialize MovmementComponent
-        MOVEMENT.rotSpeed = 40;
+        MOVEMENT.rotSpeed = 2;
 
         // Initialize SpriteComponent
         Sprite shell = goAtlas.createSprite("enemy_shell");
